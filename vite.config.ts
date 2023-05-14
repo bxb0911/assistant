454068d881d9bb0f018afc4eb41f6095af23fadc
bxb0911 <yeonlyeo@gmail.com>
2023-05-14 23:17:54 +0800

    feat: publicpath changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
-
+const isProd = process.env.NODE_ENV === 'production'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProd ? '/assistant' : '/',
   plugins: [
     vue(),
     Components({
